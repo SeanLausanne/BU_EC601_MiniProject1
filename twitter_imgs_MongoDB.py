984de452a5ecd9f418a63c612ddc8d70aab0cdae
Sean <43325298+SeanLausanne@users.noreply.github.com>
2018-12-01 01:44:51 -0500

    Update twitter_imgs_MongoDB.py

--- twitter_imgs_MongoDB.py
+++ twitter_imgs_MongoDB.py
@@ -26,7 +26,7 @@ def add_account():
     image_process.convert_pics_2_video(nPics, path)
 
     print("Analyzing the images")
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/sean/Documents/EC601/MiniProject1/EC601HW1-1f6d3fdb0675.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home//home/you_credentials_file"
     all_labels = image_process.google_vision_api(nPics)
 
     new_account = {"username": account, "nImgs": nPics, "labels": all_labels}
